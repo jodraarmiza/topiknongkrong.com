1023ae99a2528e0378c8447c1289ddbf6ebb9c89
Muhammad Jodra Armiza <145470805+jodraarmiza@users.noreply.github.com>
2026-03-22 18:58:54 +0700

    tambah sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://topik-nongkrongcom.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
